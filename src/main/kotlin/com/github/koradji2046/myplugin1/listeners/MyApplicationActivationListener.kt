ffcca910be462264bc71251e301260406c7a9c6c
GitHub Action <action@github.com>
2024-04-13 05:47:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/koradji2046/myplugin1/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/koradji2046/myplugin1/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.koradji2046.myplugin1.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
